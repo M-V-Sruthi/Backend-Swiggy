a9094b46bd2f72fcec26109eac81c8265206e5d2
sruthi <sruthi@mv.in>
2024-11-25 15:57:33 +0530

    mongoose connection changes

--- index.js
+++ index.js
@@ -14,8 +14,15 @@ const PORT = process.env.PORT || 4000;
 dotEnv.config();
 
 //mongodb connection
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("Mongodb connected successfully!"))
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, // 30 seconds timeout
+  socketTimeoutMS: 45000,    
+  })
+.then(()=>{
+    console.log("Mongodb connected successfully!")})
+
 .catch((error)=>console.log(error))
 
 app.use(bodyParser.json());//for convertion of data to json , we use bodyparser
